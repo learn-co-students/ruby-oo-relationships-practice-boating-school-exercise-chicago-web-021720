95a5c053c4a641fc75f37dc678dbb770c2ab128e
djoverbeatz29 <jroverby92@gmail.com>
2020-02-26 02:29:08 -0600

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,22 +13,25 @@ class Instructor
         @@all << self
     end
 
-    def pass_student(student, test_name)
-        rez = BoatingTest.all.find { |bt| bt.student == student && bt.test_name == test_name }
+    def find_tests
+        BoatingTest.all.find_all { |bt| bt.instructor == self }
+    end
+
+    def set_student_grade(student, test_name, new_status)
+        rez = BoatingTest.all.find { |bt| bt.student == student && bt.instructor == self && bt.test_name == test_name }
         if rez
-            rez.test_status = "passed"
+            rez.test_status = new_status
         else
-            BoatingTest.new(student, self, test_name, "passed")
+            BoatingTest.new(student, self, test_name, new_status)
         end
     end
 
+    def pass_student(student, test_name)
+        self.set_student_grade(student, test_name, "passed")
+    end
+
     def fail_student(student, test_name)
-        rez = BoatingTest.all.find { |bt| bt.student == student && bt.test_name == test_name }
-        if rez
-            rez.test_status = "failed"
-        else
-            BoatingTest.new(student, self, test_name, "failed")
-        end
+        self.set_student_grade(student, test_name, "failed")
     end
 
 end
